0d143e001e32ed9306e95dacb36c92d1c6ff167e
OLESYA MILLER <olesyam.miller@gmail.com>
2019-11-21 19:44:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,14 @@ end
 def using_insert(array, element)
   array.insert(4,element)
 end  
+
+def using_uniq(array)
+  array.uniq 
+end
   
+def using_flatten(array)
+  array.flatten 
+end
   
   
   
